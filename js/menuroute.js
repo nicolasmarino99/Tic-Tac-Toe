ca7a9059eeb0eceb55a7e9fcb0d68648a2d9304e
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-03-02 13:03:43 -0500

    add names to game

--- js/menuroute.js
+++ js/menuroute.js
@@ -3,7 +3,7 @@
 const multiplayerButton = document.getElementById('multiplayer-btn');
 // const aiButton = document.getElementById
 // const rulesButton = document.getElementById
-
+var player0_name,player1_name
 /* others selectors */
 
 const namesForm = document.getElementById('form');
@@ -20,11 +20,34 @@ multiplayerButton.addEventListener('click', () => {
   toggleHidden(namesForm);
 });
 
+
+const playerFactory = (name) => { 
+    return { name };
+};
+
+function setName(name0,name1) {
+    const player0 = playerFactory(name0) 
+
+    const player1 = playerFactory(name1) 
+
+console.log(player0,player1)
+document.getElementById('name-0').innerHTML = player0.name
+document.getElementById('name-1').innerHTML = player1.name
+
+}
+
 namesForm.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    const player0 = document.querySelector('[name="name-0"]').value;
-    const player1 = document.querySelector('[name="name-1"]').value;
+    player0_name = document.querySelector('[name="name-0"]').value;
+    player1_name = document.querySelector('[name="name-1"]').value;
     toggleHidden(menuContainer);
     toggleHidden(gameMultiplayer);
-  }
+    console.log(player0_name)
+    setName(player0_name,player1_name)
+    
+    }
 });
+
+
+
+//export {player0_name,player1_name}
--- js/players.js
+++ js/players.js
@@ -1,3 +1,14 @@
-const playerFactory = (name) => ({ 
-    name 
-});
+import { player0_name,player1_name } from './menuroute.js';
+
+
+const playerFactory = (name) => { 
+    return { name };
+};
+
+const player0 = playerFactory(player0_name) 
+
+const player1 = playerFactory(player1_name) 
+
+console.log(player0_name,player0.name)
+document.getElementById('name-0').innerHTML = player0.name
+document.getElementById('name-1').innerHTML = player1.name
