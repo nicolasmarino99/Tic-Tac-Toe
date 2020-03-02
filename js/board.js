ed45d6971d5882d75419112d3100f56dcb496242
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-03-02 16:57:54 -0500

    add grid marks click functionality

--- js/board.js
+++ js/board.js
@@ -1,22 +1,31 @@
 const boardGame = (() => {
   const board = new Array(9).fill(' ');
 
+
+  function setMovement(element) {
+    element.innerHTML = 'X'
+     
+  } 
+
   function displayBoard(arr) {
     arr.forEach((cell, i) => {
       const gridElement = document.createElement('div');
       const parentElement = document.getElementById('grid');
       gridElement.innerHTML = cell;
       parentElement.appendChild(gridElement);
+      gridElement.setAttribute('id',`grid-${i+1}`)
+      gridElement.addEventListener('click', ()=> {
+        setMovement(gridElement)
+      })
 
-      if (i % 2 === 0) {
-        gridElement.style.color = '#848AC4';
-      } else {
-        gridElement.style.color = '#ffffff';
-      }
     });
   }
 
+
   displayBoard(board);
+
+  
+
 });
 
 export { boardGame };
