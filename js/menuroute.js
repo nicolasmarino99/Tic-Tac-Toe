ee7a905ef050186485eb6af3e630821acf11de8e
Bruna <brunagenz92@gmail.com>
2020-03-02 13:00:26 -0400

    fix event listener to display/hide form and send form and display game multiplayer

--- js/menuroute.js
+++ js/menuroute.js
@@ -1,18 +1,30 @@
 /* Buttons selectors */
 
-const multiplayerButton = document.getElementById('multiplayer-btn')
-//const aiButton = document.getElementById
-//const rulesButton = document.getElementById
+const multiplayerButton = document.getElementById('multiplayer-btn');
+// const aiButton = document.getElementById
+// const rulesButton = document.getElementById
 
 /* others selectors */
 
-const namesForm = document.getElementById('form')
-
+const namesForm = document.getElementById('form');
+const menuContainer = document.getElementById('menu');
+const gameMultiplayer = document.getElementById('game-multiplayer');
 
 function toggleHidden(element) {
-    element.classList.toggle('hidden')
+  element.classList.toggle('hidden');
 }
 
 /*   ACTIONS TO BUTTONS   */
 
-multiplayerButton.addEventListener('click',toggleHidden(namesForm))
+multiplayerButton.addEventListener('click', () => {
+  toggleHidden(namesForm);
+});
+
+namesForm.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const player0 = document.querySelector('[name="name-0"]').value;
+    const player1 = document.querySelector('[name="name-1"]').value;
+    toggleHidden(menuContainer);
+    toggleHidden(gameMultiplayer);
+  }
+});
