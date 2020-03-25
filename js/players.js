031078f1d35555939ca5b673a09fdde83332f994
Bruna <brunagenz92@gmail.com>
2020-03-25 12:07:05 -0300

    intall stickler

--- js/players.js
+++ js/players.js
@@ -3,14 +3,4 @@ const playerFactory = (name, mark, score) => {
     return { name, mark, score };
 };
 
-
-/*
-const player0 = playerFactory(player0_name) 
-
-const player1 = playerFactory(player1_name) 
-
-console.log(player0_name,player0.name)
-document.getElementById('name-0').innerHTML = player0.name
-document.getElementById('name-1').innerHTML = player1.name */
-
 export { playerFactory };
