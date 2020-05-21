37e3153e2aca8fb37e0ab326f3ae73e749bb2368
Bruna <brunagenz92@gmail.com>
2020-05-21 16:32:18 -0300

    add more tests to gameModule

--- src/js/tests/gameModule.test.js
+++ src/js/tests/gameModule.test.js
@@ -4,19 +4,29 @@ import { playerFactory } from "../players.js";
 jest.mock("../players.js")
 const mockPlayer0 = {name: "Bruna", mark: "X", score: 0}
 const mockPlayer1 = {name: "Nicolas", mark: "O", score: 0}
+gameModule.setNames(["John", "Mary"])
 
 //gameModule.gameInit("Enter")
 
-test("when new game, status should be 'on'", () => {
-    gameModule.setNames(["John", "Mary"])
-    playerFactory.mockReturnValueOnce(mockPlayer0)
-
-    gameModule.newGame()
-
-    expect(gameModule.getStatus()).toBe("on")
-})
-
-test("when game starts, current player should be player 0", () => {
-    gameModule.setNames(["John", "Mary"])
-    expect(gameModule.getCurrentPlayer()).toEqual(mockPlayer0)
+describe("when new game starts", () => {
+    test("status should be 'on'", () => {
+        playerFactory.mockReturnValueOnce(mockPlayer0)
+        playerFactory.mockReturnValueOnce(mockPlayer1)
+    
+        gameModule.newGame()
+    
+        expect(gameModule.getStatus()).toBe("on")
+    })
+    
+    test("player 0 should be Bruna, with mark X", () => {
+        expect(gameModule.getPlayer0()).toEqual(mockPlayer0)
+    })
+    
+    test("player 0 should be Nicolas, with mark 0", () => {
+        expect(gameModule.getPlayer1()).toEqual(mockPlayer1)
+    })
+    
+    test("current player should be player 0", () => {
+        expect(gameModule.getCurrentPlayer()).toEqual(mockPlayer0)
+    })
 })
